fc009a226a74bf61cdbb94a8ef903b539dd4927d
Егор Лебедев <m123ry132@gmail.com>
2021-12-04 11:33:03 +0700

    fix cors - 4

--- src/main/java/kvadrakopter3/super_project/Controllers/testController.java
+++ src/main/java/kvadrakopter3/super_project/Controllers/testController.java
@@ -1,12 +1,14 @@
 package kvadrakopter3.super_project.Controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class testController {
     @GetMapping("/")
-    public String testEndPoint(){
-        return "All ok";
+    public ResponseEntity<String> testEndPoint(){
+        return new ResponseEntity<>("OK", HttpStatus.OK);
     }
 }
