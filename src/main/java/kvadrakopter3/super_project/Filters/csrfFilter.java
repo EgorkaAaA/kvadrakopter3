5707d016c3f25de48f72c930b6d86c0733fac697
Егор Лебедев <m123ry132@gmail.com>
2021-12-04 12:58:33 +0700

    (f) csrf token - 3

--- src/main/java/kvadrakopter3/super_project/Filters/csrfFilter.java
+++ src/main/java/kvadrakopter3/super_project/Filters/csrfFilter.java
@@ -5,6 +5,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -18,6 +19,7 @@ public class csrfFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
         CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
 
+        response.addCookie(new Cookie("XSRF-TOKEN",csrfToken.getToken()));
         response.setHeader("XSRF-TOKEN", csrfToken.getToken());
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Credentials", String.valueOf(true));
