0bca0ad9962af33734ff6391e4d817558316b606
Егор Лебедев <m123ry132@gmail.com>
2021-12-04 11:30:41 +0700

    fix cors - 3

--- src/main/java/kvadrakopter3/super_project/WebSecurutyConfig/WebSecConfig.java
+++ src/main/java/kvadrakopter3/super_project/WebSecurutyConfig/WebSecConfig.java
@@ -28,7 +28,7 @@ public class WebSecConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/**/post/**").authenticated()
                 .antMatchers("/**/admin/**").hasRole("ADMIN")
-                .antMatchers("localhost:3000/**").permitAll()
+//                .antMatchers("localhost:3000/**").permitAll()
                 .and()
                 .addFilterAfter(new csrfFilter(),
                         CsrfFilter.class)
