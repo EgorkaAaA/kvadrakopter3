c9f2f474e9da558d699a2ad13a00efceb2f196e6
Егор Лебедев <m123ry132@gmail.com>
2021-12-06 16:04:14 +0700

    add CsrfTokenControllerImpl in project

---
+++ src/main/java/kvadrakopter3/super_project/Controllers/CsrfTokenControllerImpl.java
@@ -0,0 +1,15 @@
+package kvadrakopter3.super_project.Controllers;
+
+import kvadrakopter3.super_project.Controllers.ControllerInterfaces.CsrfTokenControllerInterface;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CsrfTokenControllerImpl implements CsrfTokenControllerInterface {
+    @GetMapping("/csrf")
+    public ResponseEntity getCsrfToken(){
+        return new ResponseEntity<>( HttpStatus.OK);
+    }
+}
