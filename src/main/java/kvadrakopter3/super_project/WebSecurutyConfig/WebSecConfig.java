cdb858e6dc033436cd1dc0eddacf2bacf525bd18
Егор Лебедев <m123ry132@gmail.com>
2021-12-09 23:44:01 +0700

    (f) csrf disable 4

--- src/main/java/kvadrakopter3/super_project/WebSecurutyConfig/WebSecConfig.java
+++ src/main/java/kvadrakopter3/super_project/WebSecurutyConfig/WebSecConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -41,7 +42,7 @@ public class WebSecConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .csrf().disable()
                     .addFilterAfter(new csrfFilter(),
-                            OncePerRequestFilter.class)
+                            BasicAuthenticationFilter.class)
 //                    .addFilterBefore(new SameSiteFilter(), UsernamePasswordAuthenticationFilter.class)
                     .authenticationProvider(authenticationProvider())
                     .httpBasic();
