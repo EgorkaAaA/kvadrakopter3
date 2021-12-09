f495642e0ed8392c2afbc732f14c507783798d48
Егор Лебедев <m123ry132@gmail.com>
2021-12-09 17:39:54 +0700

    Exception handler for all exceptions

--- src/main/java/kvadrakopter3/super_project/ExceptionHandler/ExceptionHandlerForAllExceptions.java
+++ src/main/java/kvadrakopter3/super_project/ExceptionHandler/ExceptionHandlerForAllExceptions.java
@@ -1,15 +1,21 @@
 package kvadrakopter3.super_project.ExceptionHandler;
 
 import kvadrakopter3.super_project.Exceptions.UserNotFoundException;
+import kvadrakopter3.super_project.SuperProjectApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandlerForUserExceptions {
-    @ExceptionHandler(value = UserNotFoundException.class)
-    public ResponseEntity<ExceptionResponseBody> userNotFoundExceptionHandler(UserNotFoundException ex) {
+public class ExceptionHandlerForAllExceptions {
+    private final Logger log = LoggerFactory.getLogger(SuperProjectApplication.class);
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ExceptionResponseBody> userNotFoundExceptionHandler(Exception ex) {
+        log.error(ex.getMessage());
         return new ResponseEntity<>(new ExceptionResponseBody(ex,"error"), HttpStatus.NOT_FOUND);
     }
 }
