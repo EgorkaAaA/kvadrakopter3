45b65377d8b330d95321463ecf8f1d27a3b94c04
Егор Лебедев <m123ry132@gmail.com>
2021-12-09 22:47:27 +0700

    (f) samesite filter 4

--- src/main/java/kvadrakopter3/super_project/Filters/SameSiteFilter.java
+++ src/main/java/kvadrakopter3/super_project/Filters/SameSiteFilter.java
@@ -24,13 +24,12 @@ public class SameSiteFilter implements javax.servlet.Filter {
         boolean firstHeader = true;
         for (String header : headers) { // there can be multiple Set-Cookie attributes
             if (firstHeader) {
-                response.setHeader(HttpHeaders.SET_COOKIE, String.format("%s; %s", header, "SameSite=none"));
-                response.setHeader(HttpHeaders.SET_COOKIE, String.format("%s; %s", header, "secure"));
+                response.setHeader(HttpHeaders.SET_COOKIE, String.format("%s; %s; %s", header, "SameSite=none", "secure"));
+
                 firstHeader = false;
                 continue;
             }
             response.addHeader(HttpHeaders.SET_COOKIE, String.format("%s; %s", header, "SameSite=none"));
-            response.addHeader(HttpHeaders.SET_COOKIE, String.format("%s; %s", header, "Secure"));
         }
     }
 
