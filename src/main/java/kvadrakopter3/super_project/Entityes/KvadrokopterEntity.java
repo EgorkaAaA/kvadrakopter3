f0b86febc603096fcd9c363921c9fa2dd4cb00ec
Егор Лебедев <m123ry132@gmail.com>
2021-12-18 02:13:32 +0700

    cry

--- src/main/java/kvadrakopter3/super_project/Entityes/KvadrokopterEntity.java
+++ src/main/java/kvadrakopter3/super_project/Entityes/KvadrokopterEntity.java
@@ -1,27 +1,45 @@
 package kvadrakopter3.super_project.Entityes;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class KvadrokopterEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
-    
-    private String photoUrl;
+
+    @Column
+    @ElementCollection(targetClass=String.class)
+    private List<String> photoUrl;
 
     private String videoUrl;
 
     private String description;
+
+    @Column
+    @ElementCollection(targetClass = Integer.class)
+    @MapKeyColumn(name="Employee_Position")
+    private Map<String, Integer> specs;
+
+    public KvadrokopterEntity(String name, List<String> photoUrl, String videoUrl, String description, Map<String, Integer> specs) {
+        this.name = name;
+        this.photoUrl = photoUrl;
+        this.videoUrl = videoUrl;
+        this.description = description;
+        this.specs = specs;
+    }
 }
--- src/main/java/kvadrakopter3/super_project/Seeders/KvadrakopterSeeder.java
+++ src/main/java/kvadrakopter3/super_project/Seeders/KvadrakopterSeeder.java
@@ -4,19 +4,33 @@ import kvadrakopter3.super_project.Entityes.KvadrokopterEntity;
 import kvadrakopter3.super_project.Repositories.KvadrokopterRepo;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 @Component
 public class KvadrakopterSeeder {
     public KvadrakopterSeeder(KvadrokopterRepo kvadrokopterRepo) {
+        Map<String,Integer> map = new HashMap<>();
+        map.put("speed",100);
+        map.put("workTime",100);
+        map.put("maxHight",100);
+        map.put("cost",1000);
+        map.put("wight",500);
+        map.put("iq",7);
         IntStream.range(1,21)
                 .forEach(r -> kvadrokopterRepo.save(
                         new KvadrokopterEntity(
-                                r,
                                 "Kvadrakopter: " + r,
-                                "https://i.ytimg.com/vi/SmshlLPtEgQ/mqdefault.jpg",
+                                Arrays.asList("https://i.ytimg.com/vi/SmshlLPtEgQ/mqdefault.jpg",
+                                        "http://infocopter.ru/wp-content/uploads/2016/10/s2908-106-1-810x500.jpg",
+                                        "https://cdn.fishki.net/upload/post/2021/05/26/3769721/tn/bezymjannyj-kollazh--29.jpg",
+                                        "https://images.golos.io/DQmW7Mkv2bQBcpPownkv7xpGRGgy6aLtoH8DtF38SLzoYoh/maxresdefault.jpg",
+                                        "https://i1.i.ua/prikol/pic/9/6/1047369.jpg"),
                                 "https://www.youtube.com/watch?v=23v10-93Rq0",
-                                "fuck"
+                                "this is description be careful kvadrakopter is flight and fly",
+                                map
                         )
                 ));
     }
