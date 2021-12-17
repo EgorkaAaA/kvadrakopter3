9da0b2133dea5b8358c383810a441b78524a4332
Егор Лебедев <m123ry132@gmail.com>
2021-12-17 20:35:08 +0700

    (f) cors 1

--- src/main/java/kvadrakopter3/super_project/Entityes/RolesEntity.java
+++ src/main/java/kvadrakopter3/super_project/Entityes/RolesEntity.java
@@ -1,8 +1,6 @@
 package kvadrakopter3.super_project.Entityes;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,13 +8,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class RolesEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
     private String roleName;
 }
